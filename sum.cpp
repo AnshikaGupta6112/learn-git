44aa2fdf4bbb827b0e1cbbf49fd8049bc418a7e4
Anshika Gupta <anshika6112@gmail.com>
2023-03-25 18:31:02 +0530

    added msg in add.cpp

--- sum.cpp
+++ sum.cpp
@@ -12,7 +12,8 @@ int main() {
   sum = first_number + second_number;
 
   // prints sum 
-  cout << first_number << " + " <<  second_number << " = " << sum;     
+  cout << first_number << " + " <<  second_number << " = " << sum;
+  cout << "hello";     
 
   return 0;
 }
